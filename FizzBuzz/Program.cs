8d4af57cda31ab66b6cb5a306cebba0f2d7d6f87
neliaturquoise <nelson160698@gmail.com>
2016-08-19 23:22:07 +0300

    - Я удалила репозиторий, создала новый, добавила в него gitignore. - Доделала программу, теперь она выводит две строки.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            string f = "Fizz";
+            string b = "Buzz";
+            for (i = 1; i <= 100; i++)
+            {
+                if (i%3 == 0)
+                    Console.Write(f);
+                if (i % 5 == 0)
+                    Console.Write(b);
+                if (i % 3 != 0 && i % 5 != 0)
+                    Console.Write(i);
+                Console.WriteLine();
+             }
+            Console.ReadLine();
+        }
+    }
+}
